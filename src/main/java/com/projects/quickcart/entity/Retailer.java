55923532c95e96ef271d4eb42a2541ae7ba89999
anush <anush@LAPTOP-BNFSHRJM>
2024-09-06 16:23:21 +0530

    products view

--- src/main/java/com/projects/quickcart/entity/Retailer.java
+++ src/main/java/com/projects/quickcart/entity/Retailer.java
@@ -5,21 +5,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "retailers")
 @Getter
 @Setter
-@NoArgsConstructor
+
 public class Retailer {
 	@Id
 	@Column(name = "user_id")
 	private long userId;
-
-	public Retailer(long userId) {
-		this.userId = userId;
-	}
 
 }
