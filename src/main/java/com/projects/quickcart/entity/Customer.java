1435a3bf2d917f3045ec09afbc72cc12be89a541
SriMani-7 <basinenisrimanikanta@gmail.com>
2024-09-07 21:01:06 +0530

    Single table inheritance strategy for MyUser entity

--- src/main/java/com/projects/quickcart/entity/Customer.java
+++ src/main/java/com/projects/quickcart/entity/Customer.java
@@ -5,10 +5,10 @@ import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity
 @Getter
 @Setter
-@DiscriminatorValue("RETAILER")
-public class Retailer extends MyUser {
+@Entity
+@DiscriminatorValue("CUSTOMER")
+public class Customer extends MyUser {
 
 }
--- src/main/java/com/projects/quickcart/entity/MyUser.java
+++ src/main/java/com/projects/quickcart/entity/MyUser.java
@@ -3,20 +3,26 @@ package com.projects.quickcart.entity;
 import org.hibernate.annotations.ColumnDefault;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity
+@Entity(name = "User")
 @Table(name = "users")
 @Getter
 @Setter
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "role", discriminatorType = DiscriminatorType.STRING)
 public class MyUser {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
@@ -25,8 +31,6 @@ public class MyUser {
 	private String username;
 	@Column(nullable = false)
 	private String password;
-	@Column(nullable = false)
-	private String role;
 	@Enumerated(EnumType.STRING)
 	@ColumnDefault(value = "'PENDING_VERIFICATION'")
 	@Column(nullable = false, name = "status")
