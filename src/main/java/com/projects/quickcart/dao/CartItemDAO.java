279973b60487e9413cc279e76a7b34e36b360f5e
SriMani-7 <basinenisrimanikanta@gmail.com>
2024-09-12 09:11:15 +0530

    CartItem entity

---
+++ src/main/java/com/projects/quickcart/dao/CartItemDAO.java
@@ -0,0 +1,15 @@
+package com.projects.quickcart.dao;
+
+import java.util.List;
+
+import com.projects.quickcart.entity.Product;
+
+public interface CartItemDAO {
+	void addItem(long cutomerId, long productId);
+
+	void removeItem(long cutomerId, long productId);
+
+	List<Product> getCartProducts(long customerId);
+
+	void updateitemQuantity(long customerId, long productId, long quanitiy);
+}
---
+++ src/main/java/com/projects/quickcart/entity/CartItem.java
@@ -0,0 +1,44 @@
+package com.projects.quickcart.entity;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "cart_items")
+public class CartItem {
+	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "customer_id", insertable = false, updatable = false)
+	private Customer customer;
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "product_id", insertable = false, updatable = false)
+	private Product product;
+	@ColumnDefault("1")
+	@Column(nullable = false)
+	private int quantity = 1;
+	@EmbeddedId
+	private CartItem.CartItemId id;
+
+	@Embeddable
+	@Getter
+	@Setter
+	public static class CartItemId {
+		@Column(name = "customer_id")
+		private long customerId;
+		@Column(name = "product_id")
+		private long productId;
+	}
+}
