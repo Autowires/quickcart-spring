55cab8ccb62f846cf19502c974ae32c6f4907e87
SriMani-7 <basinenisrimanikanta@gmail.com>
2024-09-13 11:10:49 +0530

    composite primary turned into unique constraint in CartItem

--- src/main/java/com/projects/quickcart/entity/CartItem.java
+++ src/main/java/com/projects/quickcart/entity/CartItem.java
@@ -4,41 +4,33 @@ import org.hibernate.annotations.ColumnDefault;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
-import jakarta.persistence.Embeddable;
-import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "cart_items")
+@Table(name = "cart_items", uniqueConstraints = @UniqueConstraint(columnNames = { "product_id", "customer_id" }))
 public class CartItem {
-	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	@JoinColumn(name = "customer_id", insertable = false, updatable = false)
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private long id;
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "customer_id")
 	private Customer customer;
 	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-	@JoinColumn(name = "product_id", insertable = false, updatable = false)
+	@JoinColumn(name = "product_id")
 	private Product product;
 	@ColumnDefault("1")
 	@Column(nullable = false)
 	private int quantity = 1;
-	@EmbeddedId
-	private CartItem.CartItemId id;
-
-	@Embeddable
-	@Getter
-	@Setter
-	public static class CartItemId {
-		@Column(name = "customer_id")
-		private long customerId;
-		@Column(name = "product_id")
-		private long productId;
-	}
 }
