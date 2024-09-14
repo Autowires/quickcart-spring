382bb801cfed453dec0eedfea89b8f3ef8a712da
SriMani-7 <basinenisrimanikanta@gmail.com>
2024-09-14 17:55:47 +0530

    OrderDAO

---
+++ src/main/java/com/projects/quickcart/dao/OrderDAO.java
@@ -0,0 +1,21 @@
+package com.projects.quickcart.dao;
+
+import java.util.List;
+
+import com.projects.quickcart.entity.CartItem;
+import com.projects.quickcart.entity.Order;
+import com.projects.quickcart.entity.OrderDetail;
+import com.projects.quickcart.entity.PaymentDetail;
+
+public interface OrderDAO {
+
+	double getCartValue(long customerId);
+
+	List<CartItem> getCartItems(long userId);
+
+	void save(PaymentDetail paymentDetail);
+
+	void save(Order order);
+
+	void save(OrderDetail detail);
+}
---
+++ src/main/java/com/projects/quickcart/dao/impl/OrderDAOImpl.java
@@ -0,0 +1,53 @@
+package com.projects.quickcart.dao.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.projects.quickcart.dao.OrderDAO;
+import com.projects.quickcart.entity.CartItem;
+import com.projects.quickcart.entity.Order;
+import com.projects.quickcart.entity.OrderDetail;
+import com.projects.quickcart.entity.PaymentDetail;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+@Repository
+public class OrderDAOImpl implements OrderDAO {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Override
+	public double getCartValue(long customerId) {
+		return entityManager
+				.createQuery("select sum(qunatity) from CartItem c where c.customer.id = :cid", Double.class)
+				.setParameter("cid", customerId).getSingleResult();
+	}
+
+	@Override
+	@Transactional
+	public List<CartItem> getCartItems(long userId) {
+		return entityManager.createQuery("from CartItem c where c.customer.id = :id", CartItem.class)
+				.setParameter("id", userId).getResultList();
+	}
+
+	@Override
+	public void save(Order order) {
+		entityManager.persist(order);
+	}
+
+	@Override
+	public void save(PaymentDetail paymentDetail) {
+		entityManager.persist(paymentDetail);
+	}
+
+	@Override
+	public void save(OrderDetail detail) {
+		entityManager.persist(detail);
+
+	}
+
+}
